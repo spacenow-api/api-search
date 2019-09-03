dc05f63b157d4654f76d2cff336c1712841eb54a
Arthemus <arthemus.moreira@gmail.com>
2019-09-04 09:33:17 +1000

    Testing query endpoint.

--- src/functions/searchQuery.js
+++ src/functions/searchQuery.js
@@ -4,6 +4,7 @@ const r = require('./../helpers/response.utils')
 const searchService = require('./../services/search.service')
 
 module.exports.main = (event, context, callback) => {
+  console.log(event.body)
   context.callbackWaitsForEmptyEventLoop = false
   searchService
     .searchQuery(event.pathParameters.key, JSON.parse(event.body))
