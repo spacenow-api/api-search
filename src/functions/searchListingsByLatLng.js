3eb0eebb2d58db12ba04d268d00c88d9e85a8d8a
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-09-03 20:07:54 +1000

    back to original code

--- src/functions/searchListingsByLatLng.js
+++ src/functions/searchListingsByLatLng.js
@@ -1,46 +1,11 @@
 'use strict'
 
-const Sequelize = require('sequelize')
-const { DataTypes } = require('sequelize')
+const searchService = require('./../services/search.service')
 
-const r = require('./../helpers/response.utils')
-// const searchService = require('./../services/search.service')
-
-// module.exports.main = (event, context, callback) => {
-//   context.callbackWaitsForEmptyEventLoop = false
-//   searchService
-//     .searchListingIds(event.pathParameters.latlng)
-//     .then((data) => callback(null, r.success(data)))
-//     .catch((err) => callback(null, r.failure(err)))
-// }
-
-module.exports.main = async (event, context, callback) => {
-  const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_SCHEMA,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    logging: console.log
-  })
-  // const Location = require('./../models/location.model')(instance, DataTypes)
-  // console.log('Location instance ->', Location)
-  // const locations = await Location.findAll()
-  // console.log('Locations ->', locations)
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-      callback(null, r.success({ status: 'OK' }))
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-      callback(null, r.failure(err))
-    });
-  // try {
-  //   await instance.authenticate()
-  //   callback(null, r.success({ status: 'OK' }))
-  // } catch (err) {
-  //   callback(null, r.failure(err))
-  // }
+module.exports.main = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  searchService
+    .searchListingIds(event.pathParameters.latlng)
+    .then((data) => callback(null, r.success(data)))
+    .catch((err) => callback(null, r.failure(err)))
 }
