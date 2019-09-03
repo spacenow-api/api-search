c1c16e5684d0ac96a1dfd5262bf7301c22654c52
Arthemus <arthemus.moreira@gmail.com>
2019-09-03 15:42:31 +1000

    Testing environment variables.

--- src/functions/searchListingsByLatLng.js
+++ src/functions/searchListingsByLatLng.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const r = require('./../helpers/response.utils')
-const searchService = require('./../services/search.service')
+// const r = require('./../helpers/response.utils')
+// const searchService = require('./../services/search.service')
 
 module.exports.main = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false
+  // context.callbackWaitsForEmptyEventLoop = false
+  const r = require('./../helpers/response.utils')
+  const searchService = require('./../services/search.service')
   searchService
     .searchListingIds(event.pathParameters.latlng)
     .then((data) => callback(null, r.success(data)))
