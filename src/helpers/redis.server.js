c0aca68b4f4c3909769e14adbd67552c995c1061
Arthemus <arthemus.moreira@gmail.com>
2019-09-03 11:40:09 +1000

    Testing environment variables.

--- src/helpers/redis.server.js
+++ src/helpers/redis.server.js
@@ -9,7 +9,7 @@ function initInstance() {
     console.info('Initializing Redis connection.')
     redis = new Redis({
       host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT
+      port: 6379
     })
   }
 }
