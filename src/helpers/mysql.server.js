a09d7b82891807c5b26421da104c87ec01b5b9fb
Arthemus <arthemus.moreira@gmail.com>
2019-09-03 12:38:44 +1000

    Testing environment variables.

--- src/helpers/mysql.server.js
+++ src/helpers/mysql.server.js
@@ -4,21 +4,17 @@ const Sequelize = require('sequelize')
 
 let sequelize = null
 
-async function initInstance() {
+function initInstance() {
   if (!sequelize) {
     console.info('Initializing Sequelize connection.')
-    try {
-      sequelize = await new Sequelize({
-        dialect: 'mysql',
-        host: process.env.DATABASE_HOST,
-        database: process.env.DATABASE_SCHEMA,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        logging: process.env.DEBUG ? console.debug : false
-      })
-    } catch (err) {
-      console.error(err)
-    }
+    sequelize = new Sequelize({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_SCHEMA,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      logging: process.env.DEBUG ? console.debug : false
+    })
   }
 }
 
