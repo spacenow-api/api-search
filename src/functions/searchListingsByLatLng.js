d64218522f062bc4e55af5505b56642f683c4d87
Arthemus <arthemus.moreira@gmail.com>
2019-09-03 16:28:46 +1000

    Testing environment variables.

--- src/functions/searchListingsByLatLng.js
+++ src/functions/searchListingsByLatLng.js
@@ -1,14 +1,27 @@
 'use strict'
 
-// const r = require('./../helpers/response.utils')
+const Sequelize = require('sequelize')
+
+const r = require('./../helpers/response.utils')
 // const searchService = require('./../services/search.service')
 
+// module.exports.main = (event, context, callback) => {
+//   context.callbackWaitsForEmptyEventLoop = false
+//   searchService
+//     .searchListingIds(event.pathParameters.latlng)
+//     .then((data) => callback(null, r.success(data)))
+//     .catch((err) => callback(null, r.failure(err)))
+// }
+
 module.exports.main = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false
-  const r = require('./../helpers/response.utils')
-  const searchService = require('./../services/search.service')
-  searchService
-    .searchListingIds(event.pathParameters.latlng)
-    .then((data) => callback(null, r.success(data)))
-    .catch((err) => callback(null, r.failure(err)))
+  const instance = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_SCHEMA,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    logging: true
+  })
+  console.info(instance)
+  callback(null, r.success({ status: 'OK' }))
 }
