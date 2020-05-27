a87dfa85cda9b7b5b0599bba63a780ce1843a110
Lucas <csalucasnascimento@gmail.com>
2020-05-27 14:21:01 +1000

    add get payload

--- src/functions/aws/s3/put-object.js
+++ src/functions/aws/s3/put-object.js
@@ -2,11 +2,22 @@ const s3Service = require("../../../services/s3.service");
 const { success, failure } = require("../../../helpers/response.utils");
 
 module.exports.main = (event, context, callback) => {
-  console.log("BODY ===>>>", event);
+  console.log("BODY ===>>>", getPayload(event));
   context.callbackWaitsForEmptyEventLoop = false;
   const { id } = event.pathParameters;
   s3Service
-    .putObject(JSON.parse(event.body), id)
+    .putObject(getPayload(event.body), id)
     .then(data => callback(null, success(data)))
     .catch(err => callback(null, failure(err)));
 };
+
+function getPayload(data) {
+  let payload
+  try {
+    payload = JSON.parse(data)
+  } catch (ignore) {
+    const jsonFromBuffer = Buffer.from(data || '{}', 'base64')
+    payload = JSON.parse(jsonFromBuffer.toString('utf8'))
+  }
+  return payload
+}
