b99c3bae1cdd30b3285c9fcd85e8a359f327a3ee
Lucas <csalucasnascimento@gmail.com>
2020-05-27 13:26:10 +1000

    removign console log

--- src/functions/aws/s3/put-object.js
+++ src/functions/aws/s3/put-object.js
@@ -4,7 +4,6 @@ const { success, failure } = require("../../../helpers/response.utils");
 module.exports.main = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const { id } = event.pathParameters;
-  console.log("EVENT BODY ===>>>", event.body)
   s3Service
     .putObject(JSON.parse(event.body), id)
     .then(data => callback(null, success(data)))
