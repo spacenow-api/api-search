9e926ea99505ca32f9b29fb5c14341e27c2248b9
Lucas <csalucasnascimento@gmail.com>
2020-05-28 14:58:43 +1000

    serverless function changed

--- src/functions/aws/s3/put-object-from-url.js
+++ src/functions/aws/s3/put-object-from-url.js
@@ -3,8 +3,20 @@ const { success, failure } = require("../../../helpers/response.utils");
 
 module.exports.main = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
+  const { id } = event.pathParameters;
   s3Service
-    .putObjectFromURL(JSON.parse(event.body))
+    .putObjectFromURL(getPayload(event.body), id)
     .then(data => callback(null, success(data)))
     .catch(err => callback(null, failure(err)));
 };
+
+function getPayload(data) {
+  let payload
+  try {
+    payload = JSON.parse(data)
+  } catch (ignore) {
+    const jsonFromBuffer = Buffer.from(data || '{}', 'base64')
+    payload = JSON.parse(jsonFromBuffer.toString('utf8'))
+  }
+  return payload
+}
