031fcef1eeb232e0aae99eceb92fe59ca8d09af8
Lucas <csalucasnascimento@gmail.com>
2020-05-29 16:10:48 +1000

    removing s3 keys

--- src/services/s3.service.js
+++ src/services/s3.service.js
@@ -1,10 +1,7 @@
 "use strict";
 
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+const s3 = new AWS.S3();
 const axios = require("axios")
 
 const { ListingPhotos } = require('./../models')
