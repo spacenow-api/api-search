2fba0bce828917119ffbd173fa18e8c097abeb07
Lucas <csalucasnascimento@gmail.com>
2020-05-29 15:02:00 +1000

    print env vars

--- scripts/aws-cli-assumerole.sh
+++ scripts/aws-cli-assumerole.sh
@@ -9,3 +9,5 @@ temp_role=$(aws sts assume-role \
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+
+printenv | grep AWS_
